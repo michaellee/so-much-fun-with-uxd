7fd3d100a2a70193afca71bf00b6637dc4539426
Michael Lee <michaelsoolee@gmail.com>
2017-03-17 23:57:25 -0400

    Added single user route with more details

--- sample-project/app/routes/user.js
+++ sample-project/app/routes/user.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  ajax: Ember.inject.service(),
+  model(params) {
+    let self = this
+    return new Promise(function(resolve) {
+      self.get('ajax').request('http://jsonplaceholder.typicode.com/users/' + params.id, {
+        method: 'GET'
+      }).then(function(user) {
+        resolve(user)
+      })
+    })
+  }
 });
