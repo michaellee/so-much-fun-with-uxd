1c37eba7ca4cfe5d3477faf750697eef92388393
Michael Lee <michaelsoolee@gmail.com>
2017-03-21 22:32:46 -0400

    Progress

--- practical-questions/question-02.js
+++ practical-questions/question-02.js
@@ -1,33 +1,35 @@
+'use strict'
+
 function remoteMathService (cb) {
-	var  one;
- 	var  two;
-	callOneService(function (err, num) {
-		one  =  num
+	var  one
+ 	var  two
+	callOneService(function (err, num1) {
+		callTwoService(function (err, num2) {
+			one = num1
+			two = num2
+			return cb(undefined, one  +  two)
+		})
 	})
-	callTwoService(function (err, num) {
-		two  =  num
-	})
-	return  cb(undefined, one  +  two)
 }
 
 function callOneService (cb) {
 	setTimeout (function () {
- 		return  cb( undefined ,  1 );
-	}, 1000);
+ 		return cb(undefined, 1)
+	}, 1000)
 }
 
 function callTwoService (cb) {
 	setTimeout (function () {
- 		return  cb( undefined ,  2 );
-	}, 1500);
+ 		return cb(undefined, 2)
+	}, 1500)
 }
 
 remoteMathService(function (err, answer) {
 	if (err)
-		console .log ( "error " , err);
-	if  (answer  !==   3 ) {
-		console .log ( "wrong answer" , answer);
+		console.log ("error", err)
+	if (answer  !==  3 ) {
+		console.log ("wrong answer", answer)
 	} else {
-		console .log ( "correct" );
+		console.log ("correct")
 	}
 });
